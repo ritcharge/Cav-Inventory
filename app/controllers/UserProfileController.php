7ec0ac03a36ec0db28d3683e446bc89da119228a
kodumen <r.b.daitan@gmail.com>
2014-10-04 22:39:17 +0800

    Added parameters to the form.
    `userProfile.update` requires a parameter so I added one, based on the
    Laravel docs.
    
    Looking at the generated html, the URL that the form points to is in the
    correct format. However, after clicking 'Save' or the submit button,
    Laravel still throws a MethodNotAllowedExcpetion.

--- app/controllers/UserProfileController.php
+++ app/controllers/UserProfileController.php
@@ -68,7 +68,11 @@ class UserProfileController extends \BaseController {
 	 */
 	public function update($id)
 	{
-		//
+		// Check if old_password matches stored password.
+        // Check if new_password === re_password.
+        // If both are true, update password and go to user profile.
+        // If not, go back.
+        return 'UPDATE USER PASSWORD';
 	}
 
 
--- app/views/userProfile/edit.blade.php
+++ app/views/userProfile/edit.blade.php
@@ -7,7 +7,7 @@
 @section('view')
     <div><h2>Change Password</h2></div>
     <div>
-        {{ Form::open(['userProfile.update', 'class' => 'pure-form pure-form-stacked cav-form-stacked-1']) }}
+        {{ Form::open(['route' => ['userProfile.update', Auth::user()->username], 'class' => 'pure-form pure-form-stacked cav-form-stacked-1']) }}
             
         {{ Form::password('old_password', ['placeholder' => 'Current Password']) }}
         {{ Form::password('new_password', ['placeholder' => 'New Password']) }}
