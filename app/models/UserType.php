d1afec4a7e93301aab92939879f4090a3dd400c8
kodumen <r.b.daitan@gmail.com>
2014-10-05 16:26:58 +0800

    Created UserType model

---
+++ app/models/UserType.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Defines the User's type.
+ * In the future, this should also store permissions and such.
+ */
+
+class UserType extends Eloquent {
+    
+    protected $table = 'user_types';
+    
+    
+}
