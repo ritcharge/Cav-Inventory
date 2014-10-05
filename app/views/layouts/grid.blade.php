0c60df3f1078e65800f2063fbab9836129373f7b
kodumen <r.b.daitan@gmail.com>
2014-10-05 19:49:37 +0800

    Table layout, Buttons, etc. for grid and index of UsersGrid

--- app/views/layouts/grid.blade.php
+++ app/views/layouts/grid.blade.php
@@ -9,7 +9,7 @@
     </div>
     <div class="pure-g">
         <div class="pure-u-1-1">
-            <table class="pure-table pure-table-striped cav-width-1-1">
+            <table class="pure-table pure-table-horizontal pure-table-striped cav-width-1-1">
                 <thead>
                     <tr>
                         @yield('table-headers')
--- app/views/usersGrid/index.blade.php
+++ app/views/usersGrid/index.blade.php
@@ -14,3 +14,23 @@
     <th>Name</th>
     <th>Action</th>
 @stop
+
+@section('table-contents')
+    @foreach(User::all() as $user)
+        <tr>
+            <td>
+                <!--TEMPORARY. REPLACE WITH PROPER CHECKBOX THRU FORMBUILDER-->
+                <input type="checkbox" class="pure-checkbox"/>
+            </td>
+            <td>
+                {{ $user->username }}
+            </td>
+            <td>
+                {{ "$user->first_name $user->last_name" }}
+            </td>
+            <td>
+                <a class="pure-button">Edit</a>
+            </td>
+        </tr>
+    @endforeach
+@stop
