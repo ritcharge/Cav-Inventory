dee5f1fec2fcf0272605cd43db3175a82ac9444a
kodumen <r.b.daitan@gmail.com>
2014-10-08 12:14:31 +0800

    Added validation messages and made sure they are working.

--- app/views/usersGrid/create.blade.php
+++ app/views/usersGrid/create.blade.php
@@ -9,26 +9,31 @@
         <div class="pure-control-group">
             {{ Form::label('username', 'Username: ') }}
             {{ Form::text('username') }}
+            {{ $errors->first('username') }}
         </div>
         <div class="pure-control-group">
             {{ Form::label('first_name', 'First name:') }}
             {{ Form::text('first_name') }}
+            {{ $errors->first('first_name') }}
         </div>
         <div class="pure-control-group">
             {{ Form::label('last_name', 'Last name:') }}
             {{ Form::text('last_name') }}
+            {{ $errors->first('last_name') }}
         </div>
-        <div class="pure-control-group">
+        <div class="pure-control-group">    
             {{ Form::label('user_type', 'User Type:') }}
             {{ Form::select('user_type', UserType::lists('description', 'id')) }}
         </div>
         <div class="pure-control-group">
             {{ Form::label('password', 'Password:') }}
             {{ Form::password('password') }}
+            {{ $errors->first('password') }}
         </div>
         <div class="pure-control-group">
             {{ Form::label('password_confirmation', 'Re-type password:') }}
             {{ Form::password('password_confirmation') }}
+            {{ $errors->first('password_confirmation') }}
         </div>
         <div class="pure-controls">
             {{ Form::submit('Save', ['class' => 'pure-button pure-button-primary']) }}
