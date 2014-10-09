0a8e38f9f93d500d319072d85a49ec7a2e76820f
kodumen <r.b.daitan@gmail.com>
2014-10-09 23:46:04 +0800

    Added relations to Brand. Implemented dropdown for salesGrid create.

--- app/models/Brand.php
+++ app/models/Brand.php
@@ -4,6 +4,10 @@
         protected $table = 'brands';
         
         protected $fillable = ['name'];
-
+        
+        public function products() {
+            
+            return $this->hasMany('Product');
+        }
     }
 ?>
--- app/views/salesGrid/create.blade.php
+++ app/views/salesGrid/create.blade.php
@@ -14,16 +14,81 @@ Add New Sales Invoice
 {{ Form::label('customer_contact', 'Contact Number') }}
 {{ Form::input('tel', 'customer_contact') }}
 
-<h4>Item Orders</h4>
+<label>Item Orders</label>
+<div id="dropdown_group">
+    
+</div>
+
 
 <script type="text/javascript">
-
-    <?php
-        
-        
     
-    ?>
-
+    
+    addNewItemDropDown();
+    
+    /**
+     * Add a new drop down list of all products grouped by product type sorted by brand
+     */
+    function addNewItemDropDown() {
+        
+        var dropdownGroup = document.getElementById('dropdown_group');  // Div containing all dropdown elements.
+//        console.log(dropdownGroup);
+        var dropdownDiv = document.createElement('div');    // Div containing individual dropdown elements.
+        var dropdown = document.createElement('select');
+        dropdown.setAttribute('name', 'item[]');
+        dropdownDiv.appendChild(dropdown);
+        dropdownGroup.appendChild(dropdownDiv);
+        
+        <?php
+        
+        $productTypes = ProductType::orderBy('description')->get();
+        $brands = Brand::orderBy('name')->get();
+        $i = 0; // Just some counter for the var names.
+        
+        // For each product type, echo the script to make an optgroup element and append it to the other elements.
+        foreach($productTypes as $type) {    
+            
+            $i++;
+            echo // Echo script to create groups for each product type.
+            '
+            var optGroup' . $i . ' = document.createElement("optgroup");
+            optGroup' . $i . '.setAttribute("label", "' . $type->description . '");
+            dropdown.appendChild(optGroup' . $i .');
+            ';
+            
+            foreach($brands as $brand) {
+                
+                // Select products of each brand where product_type_id = $type->id
+                $productsOfBrand = $brand->products()->orderBy('product_name')->where('product_type_id', '=', $type->id)->get();
+                
+                $x = 0; // Just some counter for var name
+                
+                foreach($productsOfBrand as $product) {
+                    
+                    $x++;
+                    
+                    // Echo script to 
+                    // Create element <option>
+                    // Set attribute value to product id
+                    // Create text node brand->name + product->product_name
+                    // Append text node to <option>
+                    // Append <option> to optGroup+$i
+                    
+                    echo
+                    '
+                    var opt' . $x . ' = document.createElement("option");
+                    opt' . $x . '.setAttribute("value", ' . $product->id . ');
+                    
+                    var prodName' . $x . '=document.createTextNode("'. $brand->name . ' ' . $product->product_name . '");
+                    opt' . $x . '.appendChild(prodName' . $x . ');
+                    
+                    optGroup' . $i . '.appendChild(opt' . $x . ');
+                    ';
+                }
+            }
+        }
+        
+        ?>    
+    }
 </script>
 
 {{ Form::submit('Save') }}
