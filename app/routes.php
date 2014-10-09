7179a7ef34e6061502c8727086530ef2f030b76b
kodumen <r.b.daitan@gmail.com>
2014-10-09 18:38:11 +0800

    Cleaned up code an change some stuff to afhere to my preferences.

--- app/routes.php
+++ app/routes.php
@@ -23,37 +23,25 @@ Route::get('/login', 'SessionsController@create');
 Route::get('/logout', 'SessionsController@destroy');
 
 // USER PROFILE PAGE
-//<<<<<<< HEAD
 //Route::resource('userProfile', 'UserProfileController' );
 //Route::get('/userProfile/{userProfile}', 'UserProfileController@show')->before('auth');
 
-Route::get('inventory',function(){$inventory = InventoryLog::all();
-
-
-    return View::make('InventoryLogGrid.index');
-});
-//=======
 Route::resource('userProfile', 'UserProfileController', ['only' => ['show', 'update', 'edit']]);
 //Route::get('/userProfile/{userProfile}', 'UserProfileController@show')->before('auth');
 
 // USERS GRID
 Route::resource('usersGrid', 'UsersGridController');
-//<<<<<<< HEAD
-//>>>>>>> upstream/r-dev-branch
+
 //INVENTORY_LOG
-Route::resource('InventoryLogGrid', 'InventoryLogGridController');
-//=======
+Route::resource('inventoryLogsGrid', 'InventoryLogGridController');
 
 // PRODUCTS GRID
 Route::resource('productsGrid', 'ProductsGridController');
 
-//>>>>>>> upstream/web-dev
-
-
 //SALES PAYMENT LOG
-Route::resource('SalesPaymentLogGrid', 'SalesPaymentLogGridController');
+Route::resource('salesPaymentLogGrid', 'SalesPaymentLogGridController');
 
 //DELIVERY LOG
-Route::resource('DeliveryLogGrid', 'DeliveryLogGridController');
+Route::resource('deliveryLogGrid', 'DeliveryLogGridController');
 
 
