e811836a343ac4c1c9a08841fcc0ff2f14749818
kodumen <r.b.daitan@gmail.com>
2014-10-09 17:02:12 +0800

    Added foreach loop in index to show products. NOT TESTED.

--- app/views/productsGrid/index.blade.php
+++ app/views/productsGrid/index.blade.php
@@ -17,5 +17,15 @@ Products
 @stop
 
 @section('table-contents')
+@foreach(Product::all as $product)
 
+<tr>
+    <td><!-- CHECKBOX --></td>
+    <td>{{ '$product->brand()->name $product->name' }}</td>
+    <td>{{ $product->selling_price }}</td>
+    <td>{{ $product->quantity }}</td>
+    <td>{{ $product->type()->description }}</td>
+</tr>
+
+@endforeach
 @stop
