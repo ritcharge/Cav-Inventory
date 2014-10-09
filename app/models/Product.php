2ed80fad4f7d976645f3e8cc2e9d8e917445d53a
kodumen <r.b.daitan@gmail.com>
2014-10-09 16:26:16 +0800

    Added relationship to Product to model.

--- app/models/Product.php
+++ app/models/Product.php
@@ -8,4 +8,14 @@ class Product extends Eloquent {
     protected $table = 'products';
     
     protected $fillable = ['product_name', 'selling_price', 'quantity', 'product_type_id', 'brand_id'];
+    
+    public function brand() {
+        
+        return $this->belongsTo('Brand');
+    }
+    
+    public function type() {
+        
+        return $this->belongsTo('ProductType');
+    }
 }
