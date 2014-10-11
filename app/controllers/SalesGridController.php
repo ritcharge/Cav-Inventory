be43c5fc2e07c3c631e51c1803748e70ab1c3e57
kodumen <r.b.daitan@gmail.com>
2014-10-11 09:24:40 +0800

    Can now delete shit.

--- app/controllers/SalesGridController.php
+++ app/controllers/SalesGridController.php
@@ -87,16 +87,13 @@ class SalesGridController extends \BaseController {
 		//
 	}
 
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
 	public function destroy($id)
 	{
-		//
+		$forDelete = Input::get('for_delete');
+        
+        foreach($forDelete as $sales) Sales::find($sales)->delete();
+        
+        return Redirect::route('salesGrid.index');
 	}
 
 
